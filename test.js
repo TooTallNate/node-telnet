60a258b8f579003c2c8ab6f99da610e285495031
Nathan Rajlich <nathan@tootallnate.net>
2012-04-17 14:38:23 -0700

    repl example: use `terminal: true`

--- test.js
+++ test.js
@@ -21,23 +21,18 @@ telnet.createServer(function (client) {
   // 'readline' will call `setRawMode` when it is a function
   client.setRawMode = setRawMode
 
-  // to have 'readline' autodetect the input for "terminal" mode
-  client.isTTY = true
-
   // make unicode characters work properly
   client.do.transmit_binary()
 
   // emit 'window size' events
   client.do.window_size()
 
-  // 'readline' will call this for us
-  //client.setRawMode(true)
-
   // create the REPL
   var r = repl.start({
       input: client
     , output: client
     , prompt: 'telnet repl> '
+    , terminal: true
     , useGlobal: false
   }).on('exit', function () {
     client.end()
